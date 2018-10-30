b80c584a7dde28d421dc5fc764a8c3d59c5dc505
Raquel Smith <raquel@foodbloggerpro.com>
2018-10-29 20:05:51 -0700

    Updates

---
+++ precache-manifest.2c3037a91ec66bd805dd15959474f6b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1356d39a79e9c2c9bcf903ba3062a9e",
+    "url": "/static/media/github.c1356d39.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4a90b2670d0376e49a16",
+    "url": "/static/js/main.4a90b267.chunk.js"
+  },
+  {
+    "revision": "9e300d2b03979db5075b",
+    "url": "/static/js/1.9e300d2b.chunk.js"
+  },
+  {
+    "revision": "4a90b2670d0376e49a16",
+    "url": "/static/css/main.13fe3e56.chunk.css"
+  },
+  {
+    "revision": "9e300d2b03979db5075b",
+    "url": "/static/css/1.7743c466.chunk.css"
+  },
+  {
+    "revision": "02181cafbca92bac8ae32f80337bc981",
+    "url": "/index.html"
+  }
+];
