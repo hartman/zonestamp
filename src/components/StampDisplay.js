5927a6cb6d8670a54b1adcadf7a1386a89260142
Chico Venancio <francisco@argyle.com>
2022-05-22 09:45:05 -0300

    Add a button to change time format WIP

--- src/components/StampDisplay.js
+++ src/components/StampDisplay.js
@@ -11,19 +11,33 @@ class StampDisplay extends Component {
   state = {
     zoneName: Intl.DateTimeFormat().resolvedOptions().timeZone,
     date: parseInt( window.location.pathname.substr(1), 10 ),
-    zone: Intl.DateTimeFormat().resolvedOptions().timeZone
+    zone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    format: 'LT'
   };
+  changeFormat = () => {
+    if (this.state.format === 'h:mm a') {
+      this.setState({ format: 'HH:mm' });
+    } else {
+      this.setState({ format: 'h:mm a' });
+    }
+  }
   render() {
+
     return (
       <App
         main={
           <Fragment>
             <p className="subhead">That's...</p>
             <div className="time-hour">
-              <Moment tz={this.state.zoneName} format="h:mm a" unix>
+              <Moment tz={this.state.zoneName} format={this.state.format} unix>
 		{this.state.date}
 	      </Moment>
             </div>
+            <button
+              className="change-format"
+              value={this.state.format}
+              onClick={this.changeFormat}>24/12
+            </button>
             <div className="time-day">
               <Moment tz={this.state.zoneName} format="dddd, MMMM Do YYYY" unix>
 		{this.state.date}
