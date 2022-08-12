fb45f8e6e65f68269399e19e32507a83a571c593
Gergő Tisza <tgr.huwiki@gmail.com>
2022-08-12 15:30:30 -0700

    Add link to the Github repo
    Also fix indentation.
    
    Fixes zonestamp-toolforge#2.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,18 +9,21 @@ class Footer extends Component {
       <footer>
         <div className="cta">
           <div className="attribution">
-	    <div>
-            Made by <a href="https://github.com/hartman">Derk-Jan Hartman</a>{' '}
-            <a className="github" href="https://github.com/hartman">
-              <img alt="github logo" src={github} />
-            </a>
-	    </div>
-	    <div>
-            and <a href="https://raquelmsmith.com">Raquel M Smith</a>{' '}
-            <a className="github" href="https://github.com/raquelmsmith">
-              <img alt="github logo" src={github} />
-            </a>
-	    </div>
+            <div>
+              <a href="https://github.com/hartman/zonestamp">Zonestamp</a> was made by
+            </div>
+            <div>
+              <a href="https://github.com/hartman">Derk-Jan Hartman</a>{' '}
+              <a className="github" href="https://github.com/hartman">
+                <img alt="github logo" src={github} />
+              </a>
+            </div>
+            <div>
+              and <a href="https://raquelmsmith.com">Raquel M Smith</a>{' '}
+              <a className="github" href="https://github.com/raquelmsmith">
+                <img alt="github logo" src={github} />
+              </a>
+            </div>
           </div>
           <Link to="/?utm_source=zonestamp&utm_medium=footer&utm_campaign=create_link">Make your own zoneStamp!</Link>
         </div>
